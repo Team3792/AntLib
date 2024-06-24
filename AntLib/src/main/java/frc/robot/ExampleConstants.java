058b5690592501d15e9ce171ec539b3670c59465
keatonshall <keatonshall314@gmail.com>
2024-06-23 21:19:40 -0500

    Created full swerve subsystem with complete constants (example)

--- AntLib/src/main/java/frc/robot/ExampleConstants.java
+++ AntLib/src/main/java/frc/robot/ExampleConstants.java
@@ -4,5 +4,42 @@
 
 package frc.robot;
 
+import com.ctre.phoenix6.configs.TalonFXConfiguration;
+
+import edu.wpi.first.wpilibj.Joystick;
+import frc.lib.debugging.ControlTuning.ProfiledPIDConfig;
+import frc.lib.drivers.TalonFX.TalonFXControlConfiguration;
+import frc.lib.signal_processing.JoystickSignalProcessorConfig;
+import frc.lib.subsystem_architectures.swerve.SwerveConfiguration;
+
 /** Add your docs here. */
-public class ExampleConstants {}
+public class ExampleConstants {
+    public static class Swerve {
+        public static final SwerveConfiguration.GlobalConfig kGlobalConfig = new SwerveConfiguration.GlobalConfig(
+            0, 
+            0, 
+            0, 
+            new ProfiledPIDConfig(0, 0, 0, 0, 0)
+            );
+
+        public static final SwerveConfiguration.SharedModuleConfig kSharedModuleConfig = new SwerveConfiguration.SharedModuleConfig(
+            new TalonFXControlConfiguration.PositionConfiguration(0, 0, 0), 
+            new TalonFXControlConfiguration.VelocityConfiguration(0, 0, 0, 0, 0), 
+            0, 
+            0, 
+            0, 
+            0
+            );
+
+        public static final SwerveConfiguration.ModuleSpecificConfig kModuleConfigs[] = {
+            new SwerveConfiguration.ModuleSpecificConfig(0, 0, 0, 0),
+            new SwerveConfiguration.ModuleSpecificConfig(0, 0, 0, 0),
+            new SwerveConfiguration.ModuleSpecificConfig(0, 0, 0, 0),
+            new SwerveConfiguration.ModuleSpecificConfig(0, 0, 0, 0)
+        };
+
+        //Joystick control configs
+        public static final JoystickSignalProcessorConfig kRadialConfig = new JoystickSignalProcessorConfig(0, 0, 0);
+        public static final JoystickSignalProcessorConfig kTurnConfig = new JoystickSignalProcessorConfig(0, 0, 0);
+    }
+}
--- AntLib/src/main/java/frc/robot/RobotContainer.java
+++ AntLib/src/main/java/frc/robot/RobotContainer.java
@@ -18,6 +18,7 @@ import edu.wpi.first.wpilibj2.command.button.CommandPS5Controller;
 import frc.lib.drivers.SRXMagEncoder;
 import frc.lib.drivers.PS5Controller.AntPS5Controller;
 import frc.lib.drivers.TalonFX.AntTalonFX;
+import frc.lib.subsystem_architectures.swerve.GenericSwerveSubsystem;
 import frc.robot.Subsystems.ExampleIntakeSubsystem;
 import frc.lib.debugging.ConnectionManager;
 import frc.lib.debugging.ControlTuning.DashboardProfiledPIDTuner;
@@ -26,37 +27,29 @@ import edu.wpi.first.wpilibj.shuffleboard.Shuffleboard;
 import java.lang.String;
 
 public class RobotContainer {
-  SRXMagEncoder encoder1 = new SRXMagEncoder(0, "left encoder");
-  SRXMagEncoder encoder2 = new SRXMagEncoder(1, "right encoder");
-
   ExampleIntakeSubsystem intakeSubsystem = new ExampleIntakeSubsystem();
+  GenericSwerveSubsystem swerveSubsystem = new GenericSwerveSubsystem(
+    ExampleConstants.Swerve.kGlobalConfig,
+    ExampleConstants.Swerve.kSharedModuleConfig,
+    ExampleConstants.Swerve.kModuleConfigs
+    );
 
   AntPS5Controller controller = new AntPS5Controller(0, "Driver Controller");
-
-  AntTalonFX rightShooter = new AntTalonFX(21, "Right Shooter");
-  AntTalonFX leftShooter = new AntTalonFX(20, "Left Shooter");
-
-
-  //DashboardProfiledPIDTuner examplePIDTuner = new DashboardProfiledPIDTuner("Example Tuner");
   
   public RobotContainer() {
     ConnectionManager.start();
     configureBindings();
-
-    //Demonstration of features of AntTalonFX
-    // exampleTalonFX.addInformation(AntTalonFX.MotorInformationType.kKinematics); //Add position, velocity, and acceleration information
-    // exampleTalonFX.addInformation(AntTalonFX.MotorInformationType.kElectricity); //Add voltage and stator current information
   }
 
   private void configureBindings() {
-    controller.R1().whileTrue(new FunctionalCommand(() -> 
-    {rightShooter.resetPulse(); rightShooter.setNeutralMode(NeutralModeValue.Brake); leftShooter.resetPulse(); leftShooter.setNeutralMode(NeutralModeValue.Brake);}, 
-    () -> {rightShooter.setPulse(0.7, 0.4, 0.05, 0.1); leftShooter.setPulse(0.4, 0.7, 0.1, 0.05);}, (i) -> {rightShooter.setVoltage(0); leftShooter.setVoltage(0);}, () -> false));
-    //controller.R2().whileTrue(intakeSubsystem.getIntakeCommand(new double[] {1, 5}));
-
-    //Wait one second after game piece has been intook before handing off to other mechanism
-    intakeSubsystem.getHasGamePieceTrigger().onTrue(
-      Commands.waitSeconds(1).andThen(Commands.print("Hand Off"))
+    swerveSubsystem.setDefaultCommand(
+      swerveSubsystem.driveCommand(
+        () -> -controller.getLeftY(), 
+        () -> -controller.getLeftX(), 
+        () -> -controller.getRightX(), 
+        ExampleConstants.Swerve.kRadialConfig, 
+        ExampleConstants.Swerve.kTurnConfig, 
+        true)
     );
   }
 
